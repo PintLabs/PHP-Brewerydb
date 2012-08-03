0b0479bd2ae4f833e3044e1003d85a6450680a7d
Garrison Locke <gplocke@gmail.com>
2012-08-02 21:54:36 -0300

    Update Pintlabs/Service/Brewerydb/Exception.php
    Updated the documentation URL

--- Pintlabs/Service/Brewerydb/Exception.php
+++ Pintlabs/Service/Brewerydb/Exception.php
@@ -2,8 +2,8 @@
 /**
  * Exception class to be thrown upon receiving error from the Brewery DB API
  *
- * @see    http://brewerydb.com/api/documentation
- * @author Garrison Locke - http://pintlabs.com - @gplocke
+ * @see    http://developer.pintlabs.com/brewerydb/api-documentation/
+ * @author Pintlabs Team - http://pintlabs.com
  */
 class Pintlabs_Service_Brewerydb_Exception extends Exception
 {}
